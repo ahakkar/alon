5fb4aafe02869f8c41e647ee53d3c111c3ba23f6
Antti Hakkarainen <a.hakkarainen@iki.fi>
2023-11-30 12:51:41 +0200

    two_sets solution found from tirakirja p170

--- p1/two_sets/src/main.rs
+++ p1/two_sets/src/main.rs
@@ -3,53 +3,63 @@
 #![allow(dead_code)]
 use std::io;
 
-/**
- * 2 = ei, summa 3 
- * 3 = [1,2] [3] = 3, summa 6
- * 4 = [1,4] [2,3] = 5, summa 10
- * 5 = ei, summa 15
- * 6 = ei, summa 21
- * 7 = [2,4,6] [1,3,5,7] = 14, summa 28
- * 8 = [1,2,4,5,6] [3,7,8] = 18, summa 36
- * 9 = ei
- * 10 = ei
- * 11 = kyllä (33), summa 64
- */
 fn main() {
     let mut input = String::new(); 
     io::stdin().read_line(&mut input).unwrap_or_default();  
     let n = input.trim().parse::<i64>().unwrap_or_default();
-    let vec: Vec<i64> = (1..=n).collect(); 
+    let vec: Vec<i64> = (1..=n).rev().collect(); 
     let sum = n * (n+1) / 2;
 
     if (sum % 2 == 0) {
         println!("YES");
-        println!("{}", sum);
-        print_result(&vec);
+        divide_to_two_sets(&vec);
     } 
     else {
         println!("NO");
-        println!("{}", sum);
     }
 }
 
-fn print_result(vec: &Vec<i64>) {
+/*
+ * Divide to two sets based on "tirakirja.pdf's" NP hard problem section
+ * "Heuristiikat", page 170. 
+ * 
+ * Which said: put the numbers from largest to smallest, to the set with
+ * a smaller current sum. 
+ */
+fn divide_to_two_sets(vec: &[i64]) {
     let mut s1: Vec<i64> = vec![];
     let mut s2: Vec<i64> = vec![];
 
-    let mut add: bool = false;
+    let mut sum1:i64 = 0;
+    let mut sum2:i64 = 0;
 
     for &number in vec {
-        if add {
-            s1.push(number); 
-            add = false;
-        } else {
+        if(sum1 > sum2) {
             s2.push(number);
-            add = true;
+            sum2 += number;
+        } else {
+            s1.push(number);
+            sum1 += number;
         }
+
     }
 
-    println!("s1: {:?}, sum: {}", s1, s1.iter().sum::<i64>());
-    println!("s2: {:?}, sum: {}", s2, s2.iter().sum::<i64>());
+    print_set(&s1);
+    print_set(&s2);
+}
+
+
+/**
+ * Print the vector formatted to exercise's specification (only spaces between
+ * nums)
+ */
+fn print_set(vec: &[i64]) {
+    let formatted_string = vec.iter()
+        .map(|&num| num.to_string())
+        .collect::<Vec<String>>()
+        .join(" ");
+
+    println!("{}", vec.len());    
+    println!("{}", formatted_string);
 }
 
